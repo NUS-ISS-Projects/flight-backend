26f4897aae9ef11a0505e822a1abd66c50781429
ling jie <lingjie20@gmail.com>
2024-02-18 19:51:28 +0800

    added user registration and login APIs

--- src/main/java/com/webapp/flightsearch/FlightsearchApplication.java
+++ src/main/java/com/webapp/flightsearch/FlightsearchApplication.java
@@ -2,8 +2,12 @@ package com.webapp.flightsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.webapp.flightsearch.entity")
+@EnableJpaRepositories("com.webapp.flightsearch.repository")
 public class FlightsearchApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/webapp/flightsearch/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.webapp.flightsearch.repository;
+
+import com.webapp.flightsearch.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUserNameOrEmail(String username, String email);
+    Boolean existsByUserName(String userName);
+    Boolean existsByEmail(String email);
+}
