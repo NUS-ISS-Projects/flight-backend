042a1a53940c281229e2796685384684835dad33
ngtianxun08 <e1327862@u.nus.edu>
2024-02-18 15:37:51 +0800

    SCRUM-7 [Backend] API endpoint for locations, so that UI can get a list suggested airports based on location.
    * Added unit test for controller.

---
+++ src/test/java/com/webapp/flightsearch/FlightControllerTests.java
@@ -0,0 +1,54 @@
+package com.webapp.flightsearch;
+
+import com.amadeus.exceptions.ResponseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest(FlightController.class)
+class FlightControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AmadeusConnect amadeusConnect;
+
+    private com.amadeus.resources.Location[] mockLocations;
+
+    @BeforeEach
+    public void setup() throws ResponseException {
+        com.amadeus.resources.Location mockLocationSHA = mock(com.amadeus.resources.Location.class);
+        when(mockLocationSHA.getName()).thenReturn("HONGQIAO INTL");
+        when(mockLocationSHA.getIataCode()).thenReturn("SHA");
+
+        com.amadeus.resources.Location mockLocationPU = mock(com.amadeus.resources.Location.class);
+        when(mockLocationPU.getName()).thenReturn("PUDONG INTL");
+        when(mockLocationPU.getIataCode()).thenReturn("PU");
+
+        mockLocations = new com.amadeus.resources.Location[]{mockLocationSHA, mockLocationPU};
+        when(amadeusConnect.location("CN")).thenReturn(mockLocations);
+    }
+
+    @Test
+    public void whenCallingLocationAPIWithCountryCode_itsRespondingWithRightNumberOfAirportsAssiociated() throws Exception {
+        mockMvc.perform(get("/api/v1/locations")
+                .param("keyword", "CN")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print()) // This will print the request and response details
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.length()").value(2));
+    }
+}
+
