ab273eb352484e4b8a2e4d16d4209836e07db50f
ling jie <lingjie20@gmail.com>
2024-02-24 14:04:13 +0800

    added jwt token

--- src/main/java/com/webapp/flightsearch/controller/FlightController.java
+++ src/main/java/com/webapp/flightsearch/controller/FlightController.java
@@ -14,12 +14,14 @@ import com.webapp.flightsearch.entity.User;
 import com.webapp.flightsearch.repository.RoleRepository;
 import com.webapp.flightsearch.repository.UserRepository;
 import com.webapp.flightsearch.service.AmadeusConnect;
+import com.webapp.flightsearch.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -43,18 +45,26 @@ public class FlightController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+
     @PostMapping("/login")
     public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return new ResponseEntity<>("User login successfully!...", HttpStatus.OK);
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword()));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String token = JwtUtil.generateJwtToken(authentication);
+            return new ResponseEntity<>(token, HttpStatus.OK);
+        } catch (AuthenticationException e) {
+            return new ResponseEntity<>("Login failed: Invalid username or password.", HttpStatus.UNAUTHORIZED);
+        }
     }
 
 
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody SignUpDto signUpDto){
         // checking for username exists in a database
+
+        System.out.println("the dto is "+ signUpDto);
         if(userRepository.existsByUserName(signUpDto.getUsername())){
             return new ResponseEntity<>("Username is already exist!", HttpStatus.BAD_REQUEST);
         }
@@ -68,7 +78,7 @@ public class FlightController {
         user.setUserName(signUpDto.getUsername());
         user.setEmail(signUpDto.getEmail());
         user.setPassword(passwordEncoder.encode(signUpDto.getPassword()));
-        Role roles = roleRepository.findByName("ROLE_ADMIN").get();
+        Role roles = roleRepository.findByName("ROLE_ADMIN").orElse(null);
         user.setRoles(Collections.singleton(roles));
         userRepository.save(user);
         return new ResponseEntity<>("User is registered successfully!", HttpStatus.OK);
