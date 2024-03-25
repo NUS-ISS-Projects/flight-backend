98a61ebdf0e387a401c79b4b2df098798d1a8d8f
ngtianxun08 <155415637+ngtianxun08@users.noreply.github.com>
2024-03-25 22:34:40 +0800

    test rollback.

--- src/main/java/com/webapp/flightsearch/FlightsearchApplication.java
+++ src/main/java/com/webapp/flightsearch/FlightsearchApplication.java
@@ -11,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class FlightsearchApplication {
 
 	public static void main(String[] args) {
+		System.exit(1); // Non-zero exit code indicates error
 		SpringApplication.run(FlightsearchApplication.class, args);
 	}
 
