a3fd086526605872366ea32ad073b3deb4f9a57e
Tian Xun Ng <tian.xun.ng@hp.com>
2024-03-25 18:25:18 +0800

    test working 1.

--- src/main/java/com/webapp/flightsearch/controller/FlightController.java
+++ src/main/java/com/webapp/flightsearch/controller/FlightController.java
@@ -113,4 +113,9 @@ public class FlightController {
         FlightOfferSearch[] flights = this.amadeusConnect.flights(origin, destination, departDate, adults,children, travelClass, returnDate);
         return new ResponseEntity<>(flights, HttpStatus.OK);
     }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello World!", HttpStatus.OK);
+    }
 }
