648f95edb2f693ed2ddbdff2d5955f907bfa974c
jiayuan.ong <jiayuan.ong@fdmgroup.com>
2024-04-13 15:18:19 +0800

    Ci fixes

--- src/main/java/com/webapp/flightsearch/util/FirebaseConfig.java
+++ src/main/java/com/webapp/flightsearch/util/FirebaseConfig.java
@@ -8,6 +8,7 @@ import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -17,9 +18,15 @@ public class FirebaseConfig {
     @Bean
     public Firestore firestore() throws IOException {
         // Path to your Firebase Admin SDK JSON file
+        // Get the current working directory
+        String currentDirectory = System.getProperty("user.dir");
+
+        // Construct the file path relative to the current working directory
+        String filePath = currentDirectory + File.separator + "app" + File.separator + "google-services.json";
+
         try {
             FileInputStream serviceAccount = new FileInputStream(
-                    "app\\google-services.json");
+                    filePath);
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
