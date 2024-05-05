18786bb648a616d899aa0ff315c27ff91069892d
ngtianxun08 <155415637+ngtianxun08@users.noreply.github.com>
2024-05-05 05:12:12 +0000

    dast fix.

--- scripts/zap-script.sh
+++ scripts/zap-script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker pull owasp/zap2docker-stable
-docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://github.com/NUS-ISS-Projects/flight-backend/" -l PASS > zap_baseline_report.html
+docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://github.com/NUS-ISS-Projects/flight-backend/" -l PASS -J "-Xmx512m" -r zap_baseline_report.html
 
 echo $? > /dev/null
