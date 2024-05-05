806b4963b4111becc7be54efc8b649199272afab
ngtianxun08 <155415637+ngtianxun08@users.noreply.github.com>
2024-05-05 10:06:20 +0000

    clean up.

--- scripts/zap-script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker pull owasp/zap2docker-stable
-docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://github.com/NUS-ISS-Projects/flight-backend/" -l PASS -J "-Xmx512m" -r zap_baseline_report.html
-
-echo $? > /dev/null
